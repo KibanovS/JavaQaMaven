0027b8d7fe5d3856ee6741ffd678b8b76c716a00
Slava Kibanov <kibanchous@mail.ru>
2023-12-11 15:42:50 +0500

    again

--- src/test/java/ru/netology/services/CounterTest.java
+++ src/test/java/ru/netology/services/CounterTest.java
@@ -9,22 +9,22 @@ public class CounterTest {
         Counter service = new Counter();
         int count = 0;
         int money = 0;
-        int thrshold = 150_000;
-        int expenses = 60_000;
-        int income = 100_000;
+      int thrshold = 150_000;
+       int expenses = 60_000;
+        long income = 100_000;
         int expected = 2;
 
         for (int m = 1; m < 13; m++) {
             if (money < thrshold) {
                 //System.out.println("Месяц " + (m) + ". Денег " + (money) + ". Придётся работать. Заработал " + income + ", потратил -" + (expenses));
-                money = money + income - expenses;
+                money = (int) (money + income - expenses);
             } else {
                 //System.out.println("Месяц " + (m) + ". Денег " + (money) + ". Буду отдыхать. Потратил -" + expenses + ", затем еще -" + ((money - expenses) / 3));
                 count++;
-                money = (money - expenses) / 3;
+                money = (int) (money - expenses) / 3;
             }
         }
-        int actual = service.calculate(count);
+        int actual = service.calculate((int) income, (int) expenses);
         Assertions.assertEquals(expected, actual);
     }
 }
