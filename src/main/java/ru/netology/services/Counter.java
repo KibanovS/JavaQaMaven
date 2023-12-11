b64beeeb74c296fdc63b98a31ad29cd7a955f2f0
Slava Kibanov <kibanchous@mail.ru>
2023-12-11 18:57:55 +0500

    11

--- src/main/java/ru/netology/services/Counter.java
+++ src/main/java/ru/netology/services/Counter.java
@@ -2,16 +2,15 @@ package ru.netology.services;
 
 public class Counter {
 
-    public int calculate(int income, int expenses) {
+    public int calculate(int income, int expenses, int thrshold) {
         int count = 0;
         int money = 0;
-        long thrshold = 150_000;
         for (int m = 1; m < 13; m++) {
             if (money < thrshold) {
                 System.out.println("Месяц " + (m) + ". Денег " + (money) + ". Придётся работать. Заработал " + income + ", потратил -" + (expenses));
                 money = money + income - expenses;
             } else {
-                System.out.println("Месяц " + (m) + ". Денег " + (money) + ". Буду отдыхать. Потратил -" + expenses + ", затем еще -" + ((money - expenses) / 3));
+                System.out.println("Месяц " + (m) + ". Денег " + (money) + ". Буду отдыхать. Потратил -" + expenses + ", затем еще -" + ((money - expenses) * 2) / 3);
                 count++;
                 money = (money - expenses) / 3;
             }
--- src/test/java/ru/netology/services/CounterTest.java
+++ src/test/java/ru/netology/services/CounterTest.java
@@ -1,7 +1,6 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
 public class CounterTest {
     @org.testng.annotations.Test
@@ -9,22 +8,22 @@ public class CounterTest {
         Counter service = new Counter();
         int count = 0;
         int money = 0;
-      int thrshold = 150_000;
-       int expenses = 60_000;
-        long income = 100_000;
+        int thrshold = 150_000;
+        int expenses = 60_000;
+        int income = 100_000;
         int expected = 2;
 
         for (int m = 1; m < 13; m++) {
             if (money < thrshold) {
                 //System.out.println("Месяц " + (m) + ". Денег " + (money) + ". Придётся работать. Заработал " + income + ", потратил -" + (expenses));
-                money = (int) (money + income - expenses);
+                money = money + income - expenses;
             } else {
-                //System.out.println("Месяц " + (m) + ". Денег " + (money) + ". Буду отдыхать. Потратил -" + expenses + ", затем еще -" + ((money - expenses) / 3));
+                //System.out.println("Месяц " + (m) + ". Денег " + (money) + ". Буду отдыхать. Потратил -" + expenses + ", затем еще -" + ((money ) / 3));
                 count++;
-                money = (int) (money - expenses) / 3;
+                money = (money - expenses) / 3;
             }
         }
-        int actual = service.calculate((int) income, (int) expenses);
+        int actual = service.calculate(income, expenses, thrshold);
         Assertions.assertEquals(expected, actual);
     }
 }
